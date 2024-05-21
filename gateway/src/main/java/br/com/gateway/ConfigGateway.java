43424491ea1986e084289f2e8c8cc184157a13c4
cesar alves vecchio <ozaro.cesar@gmail.com>
2024-05-20 22:17:47 -0300

    atualizações docker compose

--- gateway/src/main/java/br/com/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/gateway/ConfigGateway.java
@@ -1,5 +1,6 @@
 package br.com.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,20 +8,28 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigGateway {
+    @Value("${ms-cliente-url}")
+    String msClienteUrl;
+    @Value("${ms-produtos-url}")
+    String msProdutosUrl;
+    @Value("${ms-pedidos-url}")
+    String msPedidosUrl;
+    @Value("${ms-logistica-entrega-url}")
+    String msLogisticaEntregaUrl;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("cliente", r -> r.path("/cliente/**")
-                        .uri("http://localhost:8081"))
+                        .uri(msClienteUrl))
                 .route("produtos", r -> r.path("/produtos/**")
-                        .uri("http://localhost:8082"))
+                        .uri(msProdutosUrl))
                 .route("pedidos", r -> r.path("/pedidos/**")
                         .uri("http://localhost:8083"))
                 .route("entrega", r -> r.path("/entregas/**")
-                        .uri("http://localhost:8084"))
+                        .uri(msPedidosUrl))
                 .route("entrega", r -> r.path("/entregadores/**")
-                        .uri("http://localhost:8084"))
+                        .uri(msLogisticaEntregaUrl))
                 .build();
     }
 
